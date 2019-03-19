e35e616c512a4648298a5b8d1a86372c8422165a
Ferris Boran <ferris.boran@gmail.com>
2019-03-19 15:15:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	pigeon_list = {}
+	data.each do |att, info|
+		info.each do |key, name|
+			name.each do |k|
+				if !pigeon_list[k]
+					pigeon_list[k] = {}
+				end
+				if !pigeon_list[k][att]
+					pigeon_list[k][att] = []
+				end
+			end
+		end
+	end
+
+	pigeon_list.each do |name, stats|
+		stats.each do |key, value|
+			data.each do |att, info|
+				if key == att
+					info.each do |x, pigg|
+						if pigg.include?(name)
+							pigeon_list[name][key] << x.to_s
+						end
+					end
+				end
+			end
+		end
+	end
+	pigeon_list
 end
