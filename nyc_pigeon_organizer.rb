d6c8d20ee46cbfd2f7a9d333be2e768760dbaf79
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-31 18:12:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, attribute_hash|
+    attribute_hash.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute] ||= []
+        pigeon_list[name][attribute].push(detail.to_s)
+        binding.pry
+      end
+    end
+  end
+  
+  pigeon_list
 end
